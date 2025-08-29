e8b814f388f7d545ae8bf4b5080a33a34ddb7399
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-08-29 14:30:40 +0000

    up

--- Operadores_log_03.c
+++ Operadores_log_03.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(){
 
-    int a = 0; // Operador logico (!) = not, Se era verdadeira vira falsa e virse e versa, ele inverte
+    int a = 1; // Operador logico (!) = not, Se era verdadeira vira falsa e virse e versa, ele inverte
 
 if (!a) //( 1(a = 0 )
 {
-    printf("a é Zero!\n");
+    printf("A Variável (a) é Zero!\n");
 }else{
-    printf("a Não é um Número Positivo!\n");
+    printf("A variável é Diferente de Zero!\n");
 }
 
 
