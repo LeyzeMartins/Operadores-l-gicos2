2a2d27ffe1eda9cfd8d7a23e922759778e4ae6d9
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-08-29 12:06:05 +0000

    up

--- Operador_Log(II)02.C
+++ Operador_Log(II)02.C
@@ -6,10 +6,10 @@ int a = 10, b = 5;
 
 if (a > 0 || b > 0)
 {
-    printf("Os Dois Números São Positivos!\n");
+    printf("Pelo Menos um dos Números Positivos!\n");
 }else
 {
-      printf("Pelo Menos um dos Números e Negativo!\n");
+      printf("oS Dois Números São Negativo!\n");
 }
 
 
