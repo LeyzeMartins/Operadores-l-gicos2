f981ca891ce0098128b0c4ea678c2bfc3e9399b7
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-08-29 14:26:48 +0000

    operadores log03

---
+++ Operadores_log_03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+    int a = 0; // Operador logico (!) = not, Se era verdadeira vira falsa e virse e versa, ele inverte
+
+if (!a) //( 1(a = 0 )
+{
+    printf("a é Zero!\n");
+}else{
+    printf("a Não é um Número Positivo!\n");
+}
+
+
+
+
+
+
+}
