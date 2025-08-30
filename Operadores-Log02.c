35d996974dc2049fa30534042a7fd3d2f0f1fc04
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-08-30 16:44:16 +0000

    operador_log02

---
+++ Operadores-Log02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int a = 10, b = 5;   // Operador (|| = OU) vai retorno verdade 1 das condições for verdadeira.
+
+if(a > 0|| b > 0){    
+
+
+  printf("Os Dois Números, São Positivos.\n");
+
+
+}else{
+
+    printf("Pelo Menos um Dos Número e Negativo.\n");
+}
+
+
+}
